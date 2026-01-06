6449edec3bb0be214621f04adf5c3f8052eabc94
Jimmy Payne <jpayne@Jimmys-Mac-mini.local>
2026-01-06 14:44:20 -0600

    Using Sonnet 3.7

--- amplify/data/bedrock.js
+++ amplify/data/bedrock.js
@@ -6,14 +6,15 @@ export function request(ctx) {
   
     // Return the request configuration
     return {
-      resourcePath: `/model/anthropic.claude-3-sonnet-20240229-v1:0/invoke`,
+      // resourcePath: `/model/anthropic.claude-3-sonnet-20240229-v1:0/invoke`,
+      resourcePath: `/model/anthropic.claude-3-7-sonnet-20250219-v1:0/invoke`,
       method: "POST",
       params: {
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          anthropic_version: "bedrock-2023-05-31",
+          anthropic_version: "bedrock-2025-02-19",
           max_tokens: 1000,
           messages: [
             {
